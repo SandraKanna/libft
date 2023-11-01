8e9f2ba06e5041ddc3a7e65ef58a13c3d8fddad4
Sandra Kanna <skanna@k1r2p6.42mulhouse.fr>
2023-11-01 20:55:10 +0100

    lstiteri OK

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -1,30 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skanna <skanna@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/01 19:38:25 by skanna            #+#    #+#             */
-/*   Updated: 2023/11/01 20:45:10 by skanna           ###   ########.fr       */
+/*   Created: 2023/11/01 20:45:50 by skanna            #+#    #+#             */
+/*   Updated: 2023/11/01 20:53:30 by skanna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstclear(t_list **lst, void (*del)(void *))
-{
-	t_list	*temp;
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{	
+	if (!lst || !f)
+		return ;
 
-	if (!lst)
-		return ;	
-	while (*lst)
-	{
-		temp = (*lst)->next;
-		if (del)
-			del((*lst)->content);
-		free(*lst);
-		*lst = temp;
+	while (lst)
+	{	
+		f(lst->content);
+		lst = lst->next;
 	}
-	*lst = NULL;
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: skanna <skanna@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 18:03:17 by skanna            #+#    #+#             */
-/*   Updated: 2023/11/01 20:40:59 by skanna           ###   ########.fr       */
+/*   Updated: 2023/11/01 20:46:45 by skanna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,5 +67,6 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
