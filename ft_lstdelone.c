ae17a87581e08597636f42d119458bf4b74fc03c
Sandra Kanna <skanna@k1r2p6.42mulhouse.fr>
2023-11-01 19:37:55 +0100

    lstdelone OK

---
+++ ft_lstdelone.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skanna <skanna@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 19:21:42 by skanna            #+#    #+#             */
+/*   Updated: 2023/11/01 19:37:31 by skanna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	if (!lst)
+		return ;
+	temp = lst->next;
+	del(lst->content);
+	free(lst);
+	lst = temp;
+}
